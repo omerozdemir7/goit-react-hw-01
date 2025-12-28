05d8d129f37aa32aa2f45ffb1efff653a07b420d
ömer özdemnir <ozd.omer17@gmail.com>
2025-12-28 16:09:55 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/goit-react-hw-01/",
+  base: "/goit-react-hw-01/", // This ensures assets are loaded from the correct path on GitHub Pages
 })
