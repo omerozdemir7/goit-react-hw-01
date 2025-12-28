14d1ecc5b39f31258cac67d24e170fa06810aaa6
Ömer ÖZDEMİR <ozd.omer17@gmail.com>
2025-12-28 16:14:35 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/goit-react-hw-01/" // This ensures assets are loaded from the correct path on GitHub Pages
+ 
 })
